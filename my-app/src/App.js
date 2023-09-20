f7d7f75492f99fe6725f169065bed3cc1acdf53e
JeonDoGyun <wjsehrbs2632@naver.com>
2023-09-20 13:03:23 +0900

    Refactor: Convert class to functional component
    class 문법으로 작성한 내용을 함수형 컴포넌트로 변경했음

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,73 @@
+import { useState } from "react";
 import Board from "./components/Board";
 
-function App() {
+const App = () => {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+
+  const status = winner
+    ? "Winner: " + winner
+    : `Next Player ${xIsNext ? "X" : "O"}`;
+
+  const handleClick = (i) => {
+    const newSquares = current.squares.slice();
+
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+    newSquares[i] = xIsNext ? "X" : "O";
+    setHistory([...history, { squares: newSquares }]);
+    setXIsNext((prev) => !prev);
+  };
+
+  const moves = history.map((step, move) => {
+    const desc = move ? `Go to move #${move}` : "Go to game Start";
+    return (
+      <li key={move}>
+        <button onClick={() => {}}>{desc}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
       </div>
-      <div className="game-info"></div>
+      <div className="game-info">
+        <div className="status">{status}</div>
+        <ol>{moves}</ol>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- my-app/src/components/Square.js
+++ my-app/src/components/Square.js
@@ -1,17 +1,11 @@
-import { Component } from "react";
 import "./Square.css";
 
-export default class Square extends Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => {
-          this.props.onClick();
-        }}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
-}
+const Square = ({ onClick, value }) => {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
